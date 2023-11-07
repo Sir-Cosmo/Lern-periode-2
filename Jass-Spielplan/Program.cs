52fd5450a5b37c1b212d477c313bb38ab9de6a49
Sir-Cosmo <142885553+Sir-Cosmo@users.noreply.github.com>
2023-11-07 07:46:10 +0100

    Add files via upload

---
+++ Jass-Spielplan/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Jass_Spielplan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anzahlTeams = 1;
+            int teams[anzahlTeams];
+            
+            Console.WriteLine("Wilkommen bei Jass Planer");
+
+            
+        }
+    }
+}
